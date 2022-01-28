8e73d586c4dd713cd11fb0dd84c37e665092dfff
YVic95 <vika.yevlentieva@gmail.com>
2022-01-28 16:50:36 +0200

    usage of RecursiveDirectoryIterator class

---
+++ iterators/recursiveIterator.php
@@ -0,0 +1,7 @@
+<?php
+    $dir = new RecursiveIteratorIterator(
+        new RecursiveDirectoryIterator('..'),
+        true);
+    foreach($dir as $file){
+        echo str_repeat("-", $dir->getDepth()) . " $file<br/>";
+    }
