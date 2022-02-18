3c60eff8e1faf708503e864ab7d7b4979f42015d
YVic95 <vika.yevlentieva@gmail.com>
2022-02-18 18:39:33 +0200

    deleting www folder

--- www/header.php
+++
@@ -1,5 +0,0 @@
-<?php
-    header("Location: http://php.net");
-    exit();
-    //header() must be called before any actual output is sent, 
-    //either by normal HTML tags, blank lines in a file, or from PHP.
